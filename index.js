7802612ccc2599f676759c9f82815effa2eb08fe
Alpacorp <alejandro.palacios88@gmail.com>
2021-12-02 21:38:46 -0500

    change images and add dynamic hero image size

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const finalText = document.getElementById('finalText');
 finalText.innerHTML = `Instituto Departamental de Cultura y Turismo | IDECUT | ${new Date().getFullYear()}`;
+
+const bannerHero = document.getElementById('bannerHero');
+const viewerWidth = () => {
+  if (screen.width < 810) {
+    bannerHero.src = './assets/images/banner_mobile.png'
+  } else {
+    bannerHero.src = './assets/images/banner_idecut_posadas-100_1.png'
+  }
+}
+
+viewerWidth();
